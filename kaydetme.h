f9e34b91e480e260df4a14c7b3b92ed5c3c0d7a4
rabiaderin1 <142705649+rabiaderin1@users.noreply.github.com>
2026-02-16 12:11:37 +0300

    Add private and public sections to kaydetme.h

--- kaydetme.h
+++ kaydetme.h
@@ -3,10 +3,12 @@
 
 #include <sqlite3.h>
 
+private:
 // Variable declarations
 extern sqlite3* veritabani;
 extern sqlite3_stmt* listele_stmt;
 
+public:
 // Function prototypes
 void dosyaAc();
 void dosyaYazdir();
